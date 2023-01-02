de3f7923c2c32a26c28474245a2a77be8a4b39b5
Jeff <4a.45.46.46@gmail.com>
2023-01-02 14:10:01 -0800

    add comments, finish bar calculations

--- media.py
+++ media.py
@@ -15,7 +15,7 @@ pose = mp_pose.Pose(
 )
 
 class PoseAnalyzer:
-    def __init__(self, in_path, out_path, session, distance_threshold, bar_window_size, contour_bbox_min, thresh_val_init, erosion_steps, contour_cutoff, thresh_val_refine, estimation_extension, round_thresh, running_avg_amount, static_image_mode, model_complexity, enable_segmentation, min_detection_confidence, min_tracking_confidence):
+    def __init__(self, video_folder, in_path, session, distance_threshold, bar_window_size, bar_stddev_threshold, contour_bbox_min, thresh_val_init, erosion_steps, contour_cutoff, round_thresh, running_avg_amount, static_image_mode, model_complexity, enable_segmentation, min_detection_confidence, min_tracking_confidence):
         self.light_red = (204, 36, 29)
         self.light_green = (152, 151, 26)
         self.light_yellow = (215, 153, 33)
@@ -36,26 +36,11 @@ class PoseAnalyzer:
         self.white = (255, 255, 255)
 
         # video setup
-        self.in_path = in_path
-        self.out_path = out_path
-
-        # parameters for back contour detection
-        self.contour_bbox_min = contour_bbox_min
-        self.thresh_val_init = thresh_val_init
-        self.erosion_steps = erosion_steps
-        self.contour_cutoff = contour_cutoff
-        self.thresh_val_refine = thresh_val_refine
-        self.estimation_extension = estimation_extension
-        self.round_thresh = round_thresh
-        self.running_avg_amount = running_avg_amount
-        self.successful_eval_count = 0
-        self.eval_list = []
-
-        self.cap = cv2.VideoCapture(self.in_path)
+        self.cap = cv2.VideoCapture(video_folder + in_path)
         self.h = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.w = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.video_output = cv2.VideoWriter(
-            filename=self.out_path,
+            filename=video_folder + in_path[:-4] + "_out.mp4",
             fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
             fps=int(self.cap.get(cv2.CAP_PROP_FPS)),
             frameSize=(self.w, self.h)
@@ -72,25 +57,56 @@ class PoseAnalyzer:
         # initialize mediapipe
         self.mp_pose = mp_pose = mp.solutions.pose
         self.pose = mp_pose.Pose(
+            # whether to reavluate on every single image
             static_image_mode=self.static_image_mode,
+            # how complex, bigger is better but slower
             model_complexity=self.model_complexity,
+            # segmentation (not used)
             enable_segmentation=self.enable_segmentation,
+            # for body to be considered detected
             min_detection_confidence=self.min_detection_confidence,
+            # for tracking to occur, rather than re-evaluation, makes it smoother
             min_tracking_confidence=self.min_tracking_confidence
         )
         self.shoulder = self.elbow = self.wrist = self.hip = self.knee = self.ankle = self.heel = self.toe = self.nose = self.eye = None
 
+        # parameters for back contour detection
+        # the minimum size of the bounding box of the contour for evaluation to occur
+        self.contour_bbox_min = contour_bbox_min
+        # thresholding value
+        self.thresh_val_init = thresh_val_init
+        # morphological operations to remove holes, requires step #
+        self.erosion_steps = erosion_steps
+        # how far to cut off from the top and left
+        self.contour_cutoff = contour_cutoff
+        # cutoff for back to be considered rounded
+        self.round_thresh = round_thresh
+        # window size for running average
+        self.running_avg_amount = running_avg_amount
+        # helper for running average
+        self.successful_eval_count = 0
+        # list of back evaluations
+        self.eval_list = []
+
         # yolo
+        # required for evaluations
         self.session = session
+        # 0: weight, 1: bar
         self.cls_names = ["weight", "bar"]
+        # colors for the classes
         self.cls_colors = {
             self.cls_names[0]: self.light_red,
             self.cls_names[1]: self.red
         }
+        # lists to store detection points
         self.bar_pt_list = []
         self.weight_pt_list = []
+        # minimum distance between points, any closer and they are removed
         self.distance_threshold = distance_threshold
+        # window size for the smoothing for the bar path
         self.bar_window_size = bar_window_size
+        # cutoff for 'good' vs 'bad' bar path
+        self.bar_stddev_threshold = bar_stddev_threshold
 
     def fully_analyze(self):
         print("initial analysis...")
@@ -122,40 +138,23 @@ class PoseAnalyzer:
 
             # EXPORT
             # write the frame # bottom left of the video
-            cv2.putText(img, f"Frame: {i}", (20, self.h - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, self.white, 1, cv2.LINE_AA)
+            cv2.putText(
+                img=img,
+                text=f"Frame: {i}",
+                org=(40, self.h - 5),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=self.white,
+                thickness=1,
+                lineType=cv2.LINE_AA
+            )
             # convert back to BGR and save the frame
             img_list.append(img)
 
         print("refining...")
-        # yolo similar keypoint rejection
-        filtered_weight_pts = self.remove_close_points(self.weight_pt_list, self.distance_threshold)
-        filtered_bar_pts = self.remove_close_points(self.bar_pt_list, self.distance_threshold)
-        # s_f_weight_pts = self.smooth_points(filtered_weight_pts, self.bar_window_size)
-        # s_f_bar_pts = self.smooth_points(filtered_bar_pts, self.bar_window_size)
-        for i in tqdm(range(len(img_list))):
-            img = img_list[i]
-            cv2.polylines(
-                img=img,
-                pts=[np.array(filtered_weight_pts)],
-                isClosed=False,
-                color=self.light_red,
-                thickness=5,
-                lineType=cv2.LINE_AA
-            )
-            cv2.polylines(
-                img=img,
-                pts=[np.array(filtered_bar_pts)],
-                isClosed=False,
-                color=self.red,
-                thickness=5,
-                lineType=cv2.LINE_AA
-            )
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            self.video_output.write(img)
+        self.full_yolo_process(img_list)
 
-        self.cap.release()
-        self.video_output.release()
-
+    # define keypoints from mediapose with variable names
     def read_keypoints(self, lm, lmPose):
         if (lm is None):
             print('fail')
@@ -172,6 +171,7 @@ class PoseAnalyzer:
         self.eye      = [lm.landmark[lmPose.RIGHT_EYE].x        * self.w, lm.landmark[lmPose.RIGHT_EYE].y        * self.h]
         self.right_ear = [lm.landmark[lmPose.RIGHT_EAR].x       * self.w, lm.landmark[lmPose.RIGHT_EAR].y       * self.h]
 
+    # draw lines between keypoints, e.g. shoulder-elbow...
     def draw_lines(self, img):
         cv2.line( # NOSE - EYE
             img=img, thickness=3, lineType=cv2.LINE_AA,
@@ -234,6 +234,7 @@ class PoseAnalyzer:
             color=self.light_green,
         )
 
+    # draw points on keypoints, e.g. nose, eye...
     def draw_points(self, img):
         cv2.circle( # NOSE
             img=img, thickness=-1, lineType=cv2.LINE_AA,
@@ -286,6 +287,7 @@ class PoseAnalyzer:
             radius=5, color=self.green,
         )
 
+    # evaluation and drawing of the back contour
     def back_contour(self, img, img_orig):
 
 # INITIAL HIP-SHOULDER BOUNDING BOX
@@ -448,14 +450,20 @@ class PoseAnalyzer:
             lineType=cv2.LINE_AA
         )
 
-    # determine the intersection between a line (defined by two points) and contour (defined by alist of points)
+    # helper for back contour
+    # determine the intersection between a line (defined by two points) and contour (defined by a list of points)
     def determine_intersect(self, line_endpoints, contour_pts):
         slope = (line_endpoints[1][1] - line_endpoints[0][1]) / (line_endpoints[1][0] - line_endpoints[0][0])
         intercept = line_endpoints[0][1] - slope * line_endpoints[0][0]
+        # define y and m
         intersections = []
+        # look through every contour point, if it lies close enough to the line, add it to the list of intersections
         for point in contour_pts:
             if abs(point[1] - (slope * point[0] + intercept)) < 3:
                 intersections.append(point)
+        # if there is an intersection, get the ones closer to the bottom right
+        # this assumes that the user is facing right, this makes sure the intersection lies on the user
+        # as opposed to being some random point on the contour caused by thresholding
         if len(intersections) > 0:
             closest_intersect = intersections[0]
             for point in intersections:
@@ -483,6 +491,7 @@ class PoseAnalyzer:
         im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
         return im, r, (dw, dh)
 
+    # drawing the detected bounding boxes every frame
     def yolo_annotate(self, img, img_orig):
         image, ratio, dwdh = self.yolo_pre(img_orig, auto=False)
         image = image.transpose((2, 0, 1))
@@ -494,7 +503,6 @@ class PoseAnalyzer:
         inname = [i.name for i in self.session.get_inputs()]
         inp = {inname[0]:im}
         outputs = self.session.run(outname, inp)[0]
-        ori_images = [img_orig]
         for i,(batch_id,x0,y0,x1,y1,cls_id,score) in enumerate(outputs):
             box = np.array([x0,y0,x1,y1])
             box -= np.array(dwdh*2)
@@ -513,6 +521,57 @@ class PoseAnalyzer:
                 thickness=3
             )
 
+    # remove overlapping points, draw paths, calculate straightness, write to screen
+    def full_yolo_process(self, img_list):
+        filtered_bar_pts = self.remove_close_points(self.bar_pt_list, self.distance_threshold)
+        if (len(filtered_bar_pts) == 0):
+            return
+        filtered2_bar_pts = self.remove_outliers(filtered_bar_pts, 50)
+        s_f_bar_pts = self.smooth_horizontally(filtered2_bar_pts, self.bar_window_size)
+        np_s_f_bar_pts = np.array(s_f_bar_pts)
+        x_coords = np_s_f_bar_pts[:, 0]
+        median_x = sorted(x_coords)[len(x_coords) // 2]
+        min_y = min(np_s_f_bar_pts[:, 1])
+        max_y = max(np_s_f_bar_pts[:, 1])
+        stddev = round(np.std(x_coords - median_x), 1)
+        text_color = self.light_red if stddev > self.bar_stddev_threshold else self.light_aqua
+
+        for i in tqdm(range(len(img_list))):
+            img = img_list[i]
+            cv2.putText(
+                img=img,
+                text=f"bar: {stddev}",
+                org=(self.w - 160, self.h - 5),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=text_color,
+                thickness=2,
+                lineType=cv2.LINE_AA
+            )
+            cv2.polylines(
+                img=img,
+                pts=[np_s_f_bar_pts],
+                isClosed=False,
+                color=self.red,
+                thickness=5,
+                lineType=cv2.LINE_AA
+            )
+            cv2.line(
+                img=img,
+                pt1=(median_x, min_y),
+                pt2=(median_x, max_y),
+                color=self.light_aqua,
+                thickness=3,
+                lineType=cv2.LINE_AA
+            )
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            self.video_output.write(img)
+
+        self.cap.release()
+        self.video_output.release()
+
+    # helper for path drawing, remove all points that are too close to each other in the y axis
+    # eliminates bar staying still and stationary weights
     def remove_close_points(self, points, threshold):
         filtered_points = []
         for i, p1 in enumerate(points):
@@ -520,29 +579,38 @@ class PoseAnalyzer:
             for j, p2 in enumerate(points):
                 if i == j:
                     continue
-                if math.hypot(p1[0] - p2[0], p1[1] - p2[1]) < threshold:
+                # don't add points that are too close together
+                if abs(p1[1] - p2[1]) < threshold:
                     should_add_point = False
                     break
             if should_add_point:
                 filtered_points.append(p1)
         return filtered_points
 
-    def smooth_points(self, points, window_size):
+    # helper for path drawing, remove all points that are too far from the median in the x axis
+    def remove_outliers(self, points, threshold):
+        x_coords = [p[0] for p in points]
+        median_x = sorted(x_coords)[len(x_coords) // 2]
+        # remove points too far away from the x median
+        filtered_points = [p for p in points if abs(p[0] - median_x) < threshold]
+        return filtered_points
+
+    # helper for path drawing, interpolate the bar path, but only horizontally
+    def smooth_horizontally(self, points, window_size):
         smoothed_points = []
         for i in range(len(points)):
-            # Compute the average of the point and its neighbors
-            average = points[0]
-            num_neighbors = 1
-            for j in range(1, window_size + 1):
-                if i - j >= 0:
-                    average = tuple(map(sum, zip(average, points[i - j])))
+            # compute the average of the x-coordinates within the window
+            average_x = 0
+            num_neighbors = 0
+            for j in range(-window_size, window_size + 1):
+                if i + j >= 0 and i + j < len(points):
+                    average_x += points[i + j][0]
                     num_neighbors += 1
-                if i + j < len(points):
-                    average = tuple(map(sum, zip(average, points[i + j])))
-                    num_neighbors += 1
-            average = tuple(x / num_neighbors for x in average)
-            smoothed_points.append(average)
-        return np.array(smoothed_points, np.int32)
+            average_x /= num_neighbors
+            # y is kept the same
+            y = points[i][1]
+            smoothed_points.append((int(points[i][0]), y))
+        return smoothed_points
 
 
 # todo:
@@ -558,19 +626,18 @@ providers = ['CUDAExecutionProvider']
 session = ort.InferenceSession("./models/yolov7_weight.onnx", providers=providers)
 
 pose_analyzer = PoseAnalyzer(
-    in_path='./videos/deadlift.mp4',
-    out_path='./videos/deadlift_out.mp4',
+    video_folder='./videos/',
+    in_path='rdl.mp4',
 
     session=session,
     distance_threshold=2,
     bar_window_size=2,
+    bar_stddev_threshold=5,
 
     contour_bbox_min=20,
     thresh_val_init=200,
-    thresh_val_refine=210,
     erosion_steps=4,
     contour_cutoff=10,
-    estimation_extension=1,
     round_thresh=0.16,
     running_avg_amount=2,
 
